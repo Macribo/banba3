bc6abe28aa95821c2b2b84baee03e5b0d023c408
ribo <roibeard.mac.unfraidh@gmail.com>
2021-02-24 10:13:28 +0100

    Add routes to main view: '/','/champions'

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+
+} from "react-router-dom";
 
 export default class MainView extends React.Component {
 
 	constructor() {
-	  super();
-	  this.state = {}}
-	
-	render(){
+		super();
+		this.state = {
 
-		return(
-			<h1>Greetings from main</h1>
+			tallyA: 1,
+			tallyB: 0,
+			tallyX: 0,
+			tallyY: 0,
+
+		}
+	}
+
+	render() {
+
+		return (
+			<Router>
+				<Route exact path="/">
+					<>
+						<h1>Greetings from main</h1>
+						<h2>tallyA: {this.state.tallyA}</h2>
+						<h2>tallyB: {this.state.tallyB}</h2>
+						<h2>tallyX: {this.state.tallyX}</h2>
+						<h2>tallyY: {this.state.tallyY}</h2>
+					</></Route>
+
+									<Route exact path="/champions">
+					<>
+						<h1>Greetings from route champions</h1>
+						<h2>tallyA: {this.state.tallyA}</h2>
+						<h2>tallyB: {this.state.tallyB}</h2>
+						<h2>tallyX: {this.state.tallyX}</h2>
+						<h2>tallyY: {this.state.tallyY}</h2>
+					</></Route></Router>
 		)
 	}
-	}
+}
