4de509b2a00ac47f1119d62a88a0cc19bf7067e3
ribo <roibeard.mac.unfraidh@gmail.com>
2021-03-09 20:15:54 +0100

    Create basic game map. Connect to react.

--- src/components/baile/baile.jsx
+++ src/components/baile/baile.jsx
@@ -1,92 +1,97 @@
 import React from 'react';
-import './baile.css'
+import './baile.css';
+import $ from 'jquery';
+
 export class Baile extends React.Component {
 
-	constructor() {
-		super();
-		this.state = {
-		}
+    constructor() {
+        super();
+        this.state = {
+        }
 
-		var gameMap =
-        [
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 0, 0, 0, 0, 1, 1, 1],
-            [1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
-            [1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
-            [1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
-            [1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
-            [1, 1, 1, 0, 0, 0, 0, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-        ];
+    }
 
-		var gameObjects =
-        [
+    jQueryCode = () => {
+
+        var map = [
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, "P", 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ];
-		var PLAYER = "P";
-	
-	
-	    var playerRow;
-    var playerColumn;
-	var ROWS = gameMap.length;
-    var COLUMNS = gameMap[0].length;
 
-    for (var row = 0; row < ROWS; row++) {
-        for (var column = 0; column < COLUMNS; column++) {
-            if (gameObjects[row][column] === PLAYER) {
-                playerRow = row;
-                playerColumn = column;
+        //Map code
+        var WATER = 0;
+        var ISLAND = 1;
+        var PIRATE = 2;
+        var HOME = 3;
+        //The size of each cell
+        var SIZE = 10;
+        //The number of rows and columns
+        var ROWS = map.length;
+        var COLUMNS = map[0].length;
+        refresh();
+        function refresh() {
+            //Clear the stage of img tag cells
+            //from the previous turn
+
+            //refresh the game by looping through the map arrays
+            for (var row = 0; row < ROWS; row++) {
+                for (var column = 0; column < COLUMNS; column++) {
+                    //Create an img tag called cell
+                    var cell = document.createElement("img");
+                    //Set its CSS class to "cell"
+                    cell.setAttribute("class", "cell");
+                    //Add the img tag to the <div id="stage"> tag
+                    $('#stage').append(cell);
+                    //Find the correct image for this map cell
+                    switch (map[row][column]) {
+                        case WATER:
+                            cell.src = "../images/water.png";
+                            break;
+                        case ISLAND:
+                            cell.src = "../images/island.png";
+                            break;
+                        case PIRATE:
+                            cell.src = "../images/pirate.png";
+                            break;
+                        case HOME:
+                            cell.src = "../images/home.png";
+                            break;
+                        default: break
+                    }
+                    //Position the cell
+                    cell.style.top = row * SIZE + "%";
+                    cell.style.left = column * SIZE + "%";
+                }
             }
 
 
 
         }
-        // gameObjects[playerRow][playerColumn] = '../img/characters/feitheamh1.gif'
-
+    }
+    componentDidMount() {
+        this.jQueryCode();
     }
 
-	}
+    render() {
+        return (
+            <>
 
+                <div className="container">
+                    <div id="stage"></div>
+                    <p id="output"></p>
+                </div>
+            </>
 
-
-
-	render() {
-		return (
-			<>
-
-            
-
-				<div className="baile">
-
-					<div className="sea"></div>
-					<div className="countyMap"></div>
-
-					<div id="middle-hist" className="justify-content-center">
-
-						<div id="stageBG">
-{}
-						</div>
-						<div id="tall-order" className="justify-content-center">
-							<div id="stage"></div>
-
-						</div>
-
-					</div>
-				</div>
-			</>
-
-		)
-	}
+        )
+    }
 
 }
